9b897917b214035110c379575f5347d4576adabd
Tejas Nambiar <justanotherapp-1@outlook.com>
2026-08-16 17:13:24 +0530

    app file changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {
+    //     path: '',
+    //     component: MainLayoutComponent,
+    //     children: [
+    //         {
+    //             path: '',
+    //             redirectTo: 'users',
+    //             pathMatch: 'full'
+    //         },
+    //         {
+    //             path: 'users',
+    //             // loadComponent: () => import('./features/users/users.component').then(m => m.UsersComponent) 
+    //         }
+    //     ]
+    // }
+];
